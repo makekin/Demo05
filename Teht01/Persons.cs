05ef712d881730cab7f0ae50fbf5f6b810b36070
k2751 <k2751@student.jamk.fi>
2017-02-09 10:59:30 +0200

    Demo05

---
+++ Teht01/Persons.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Persons
+    {
+        //persons collection stores person-objects 
+        private List<Person> persons;
+
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+
+        /// <summary>
+        /// Add persons to collection
+        /// </summary>
+        /// <param name="person">person to add</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count)
+            {
+                return persons.ElementAt(index);
+            }
+            else return null;
+        }
+
+        public Person FindPerson(string SocialSecurityNumber)
+        {
+            foreach (Person person in persons)
+            {
+                //compare two string with Equals
+                if (SocialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person;
+                }
+            }
+            return null; //person with search socialsecuritynumber not found in collection
+        }
+
+        //prints data in persons
+        public void PrintData()
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person); //kirjoittaa person.ToString vastaavan
+            }
+        }
+
+    }
+}
--- Teht01/Program.cs
+++ Teht01/Program.cs
@@ -10,6 +10,60 @@ namespace Teht01
     {
         static void Main(string[] args)
         {
+            //create persons (collection)
+            Persons myFriends = new Persons();
+
+            //create a few Person objects
+            Person person1 = new Person
+            {
+                Firstname = "Kirsi",
+                Lastname = "Kernel",
+                SocialSecurityNumber = "121212-1212"
+            };
+            Person person2 = new Person
+            {
+                Firstname = "Matti",
+                Lastname = "Mainio",
+                SocialSecurityNumber = "131313-1313"
+            };
+            Person person3 = new Person
+            {
+                Firstname = "Jussi",
+                Lastname = "Juonio",
+                SocialSecurityNumber = "242424-2424"
+            };
+
+            //add Person objects to Persons (myfriends)
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            //get one person
+            Person person4 = myFriends.GetPerson(0);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("There is no person at that position!");
+            }
+
+            //print collection
+            myFriends.PrintData();
+
+            //find person
+            string socialSecurityNumber = "131313-1313";
+            Console.WriteLine("find person with ssn: " + socialSecurityNumber);
+            Person person5 = myFriends.FindPerson(socialSecurityNumber);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Can't find person with that ssn!");
+            }
         }
     }
 }
